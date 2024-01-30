59c36a61ac260cf699ec8c2fda89df2cb98ea81a
programmingquestioner <contact.mousom@gmail.com>
2024-01-30 20:18:34 +0600

    [Shift](File) Shift db config file from config folder

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const port = process.env.PORT || 5000;
 const http = require('http')
 const app = require('./src/app');
 const server = http.createServer(app)
-const {run} = require('./src/config/dbConfig');
+const {run} = require('./src/db/dbConfig');
 
 run().catch(console.dir);
