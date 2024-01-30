2c085cbb59f91643bff701e6ab750d1f82708bcb
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-01-31 03:25:22 +0600

    [change](URL structure) version control maintain with api endpoint

--- src/routes/adoptPage/index.js
+++ src/routes/adoptPage/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const adoptPage=require('../../api/adoptPageApplication/controller/adoptPagePost')
 const getdata = require('../../api/adoptPageApplication/controller/adotPageGet')
 
-router.post('/adoptData',adoptPage)
-router.get('/adoptData',getdata)
+router.post('/api/v1/adoptData',adoptPage)
+router.get('/api/v1/adoptData',getdata)
 
 
 module.exports = router
