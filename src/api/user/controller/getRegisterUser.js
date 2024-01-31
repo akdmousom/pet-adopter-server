27b6b06801e4e92c2d242a3eeb7964b4e4791f15
programmingquestioner <contact.mousom@gmail.com>
2024-01-31 20:15:52 +0600

    [Change](API) Handle api response if the response is null our api send a response with json format message

--- src/api/user/controller/getRegisterUser.js
+++ src/api/user/controller/getRegisterUser.js
@@ -4,13 +4,13 @@ const getRegisterUser = async(req,res,next)=>{
     const client = mongoClient();
     const connectDB = client.db('insertDB').collection('registerdUser')
     const {email} = req.query;
-    console.log(email);
     const filter = {
         userEmail: email
     }
     const userRegistration = await connectDB.findOne(filter);
     const cursor = await userRegistration
     console.log(cursor);
+    if (cursor == null) return res.json({message: "Data Not Exsist"});
     res.send(cursor)
 }
 
