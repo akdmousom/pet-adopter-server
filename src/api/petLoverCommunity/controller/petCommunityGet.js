fb08ef8127abcc184e7d0ec80de11b8e2cba9b07
lokman hakim <lokmanhakimshawon3@gmail.com>
2024-02-09 15:10:40 +0600

    [create](petCommunity api) create petCommunity api for post and get data

---
+++ src/api/petLoverCommunity/controller/petCommunityGet.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoClient = require('../../../utils/mongoDB');
+const [client, ObjectId] = mongoClient()
+
+const petCommunityGetData = async(req,res,next)=>{
+    const petCommunity = client.db("insertDB").collection("petCommunity");
+   
+    const cursor = await petCommunity.find().toArray();
+    const result = await cursor;
+    res.send(result)
+
+}
+module.exports = petCommunityGetData
