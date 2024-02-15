da005282cb0b75e95a84dc2b0fd8d15d2127c6db
programmingquestioner <contact.mousom@gmail.com>
2024-02-16 01:39:34 +0600

    [Added](Comment) Added comment on index.js file and inform about the packages

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// Added dotenv package
 require('dotenv').config();
+
 const port = process.env.PORT || 5000;
+
+// Require http api from default node js
 const http = require('http')
+
+
 const app = require('./src/app');
+
 const server = http.createServer(app)
 const {run} = require('./src/db/dbConfig');
 
