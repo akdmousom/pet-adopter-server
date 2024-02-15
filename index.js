e9ebf3db2def928a2b2df4e7cfb481c40cd16021
programmingquestioner <contact.mousom@gmail.com>
2024-02-16 01:52:43 +0600

    [Add](Comment) Added comment on index file and share info about which file incule here from where

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const port = process.env.PORT || 5000;
 // Require http api from default node js
 const http = require('http')
 
-
+// This app come from src folder.
 const app = require('./src/app');
 
+//This code help us to build a server
 const server = http.createServer(app)
+
+// Import the mongo db run method
 const {run} = require('./src/db/dbConfig');
 
 run().catch(console.dir);
