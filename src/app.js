fb53846ab932f0e208486a501aea079cc8b5aca3
programmingquestioner <contact.mousom@gmail.com>
2024-02-16 00:46:05 +0600

    [Add](Post Like Api) If any user like the post the blog post like count +1

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const userRegister = require('./routes/user/index')
 const petlistings = require('./routes/petlistings')
 const donation = require('./routes/donation');
 const userAction = require('./routes/userAction')
+const blogPost = require('./routes/blogs')
 
 // Apply Middleware Through This Line
 corsMiddleware(app);
@@ -39,6 +40,8 @@ app.use(donation);
 // User Action 
 
 app.use(userAction);
+
+app.use(blogPost)
 
 
 
