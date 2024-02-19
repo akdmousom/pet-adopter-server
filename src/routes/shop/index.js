9c348315a17458b3f6715d8e3e652d4613b51511
rasel khan <tunnerbd2016@gmail.com>
2024-02-19 22:04:36 +0600

    create order get and post api when user buy clicked then user and item details save database

--- src/routes/shop/index.js
+++ src/routes/shop/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const shoppost = require('../../api/shop/controller/shoppost')
+const orderInfopost = require('../../api/shop/controller/orderInfopost')
+const orderInfoget = require('../../api/shop/controller/orderInfoget')
 const shopget = require('../../api/shop/controller/shopget')
 const singleitem = require('../../api/shop/controller/singleitem')
 const router = express.Router();
@@ -7,6 +9,8 @@ const router = express.Router();
 
 
 router.post('/api/v1/shopitems', shoppost)
+router.post('/api/v1/orders', orderInfopost)
+router.get('/api/v1/orders', orderInfoget)
 router.get('/api/v1/shopitems', shopget)
 router.get('/api/v1/item/:id', singleitem)
 
