469a256f2ff44b8b5a9df7586eec35c1a2df5a0b
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-03-03 14:17:21 +0600

    [sendMail] add maul system in this website

---
+++ src/utils/SendMail/index.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer')
+const { USER_EMAIL, USER_PASS } = require('../../config')
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'shaharulsiyam0273@gmail.com',
+        pass: 'rfmdmchofhjsfenx',
+    },
+})
+const mailSend = async (mailData) => {
+    const mailOptions = {
+        from: mailData.sender,
+        to: mailData.receiver,
+        subject: mailData.subject,
+        html: mailData.msg,
+    };
+    const info = await transporter.sendMail(mailOptions)
+    // console.log(info)
+}
+module.exports = mailSend
