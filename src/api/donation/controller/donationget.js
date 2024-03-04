df1532fd1477638a1c4b80f7ae56f3da16983ed1
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-03-04 12:24:35 +0600

    [donation] get and post api for donation

--- src/api/donation/controller/donationget.js
+++ src/api/donation/controller/donationget.js
@@ -1,15 +1,9 @@
-
-
 const mongoClient = require('../../../utils/mongoDB');
-
 const get = mongoClient()
 const [client, ObjectId] = get
-const donationpost = async (req, res, next) => {
-    // const client = mongoClient();
+const donationget = async(req,res)=>{
     const donationDB = client.db('petAdopterDB').collection("donations")
-    const query = req.body;
-    const result = await donationDB.insertOne(query);
+    const result = await donationDB.find().toArray();
     res.send(result)
 }
-
-module.exports = donationpost
+module.exports = donationget
--- src/routes/donation/index.js
+++ src/routes/donation/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const donationpost = require('../../api/donation/controller/donationpost')
+const donationpost = require('../../api/donation/controller/donationpost');
+const donationget = require('../../api/donation/controller/donationget');
 
 
 router.post('/api/v1/donations', donationpost)
+router.get('/api/v1/donations', donationget)
 module.exports = router
