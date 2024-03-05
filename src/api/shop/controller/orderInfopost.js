94f5d161fc01ff0cc061dba66f60606ceac1bd64
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-03-05 19:52:41 +0600

    [donation] get api for single users donation

--- src/api/shop/controller/orderInfopost.js
+++ src/api/shop/controller/orderInfopost.js
@@ -9,13 +9,13 @@ const orderinfopost = async (req, res, next) => {
     const mailData = {
         sender: 'shaharulsiyam0273@gmail.com',
         receiver: `${query?.email}`,
-        subject: 'your order has been confirmed',
+        subject: 'your order has been placed',
         msg: `<body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 20px; color: #555;">
             <p>Dear ${query.fullName},</p>
             <p>your order has been placed</p>
             <h3 style="color: #333;"> Details:</h3>
             <ul style="list-style-type: none; padding: 0;">
-                <li style="margin-bottom: 10px;"><strong>Employee price:</strong> ${query.price}</li>
+                <li style="margin-bottom: 10px;"><strong>item price:</strong> ${query.price}</li>
                 <li style="margin-bottom: 10px;"><strong>item:</strong> ${query.itemName}</li>
                 <li style="margin-bottom: 10px;"><strong>transiotion ID :</strong> ${query.transID}</li>
             </ul>
